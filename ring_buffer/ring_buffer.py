ad2911e66ee9509df00ece8f23c19e64574729cb
Jeremy Morales <jeremy.morales@gmail.com>
2020-06-01 16:19:10 -0700

    ring buffer half done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,35 @@
+from double import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        #if empty
+        ## add to tail
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+        #if full 
+        elif self.storage.length == self.capacity :
+            # if current is None, 
+            if self.current == self.current.head:
+                self.current = self.storage.head.next
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+            
+            # if full, current is tail, set to head
+
+            elif self.current == self.storage.tail:
+                self.storage.remove_from_tail()
+                self.storage.add_to_tail(item)
+                self.current = self.current.head
+                
+
+        #storage full, current is not tail 
 
     def get(self):
         pass
+
